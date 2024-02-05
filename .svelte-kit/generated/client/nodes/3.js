1bb57d732928b68a3b8a7e1d05e35492c26cfa4c
MeguminChan830 <huynhbaophuc192837@gmail.com>
2024-02-05 17:17:58 +0700

    First commit

---
+++ .svelte-kit/generated/client/nodes/3.js
@@ -0,0 +1,3 @@
+import * as universal from "../../../../src/routes/[id=something]/+page.js";
+export { universal };
+export { default as component } from "../../../../src/routes/[id=something]/+page.svelte";
