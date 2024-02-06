3ea8abfd1980c8406afb2a5a03fc02dd9389ee33
MeguminChan830 <huynhbaophuc192837@gmail.com>
2024-02-06 13:00:47 +0700

    After done ui

---
+++ src/lib/components/ui/dialog/dialog-description.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { Dialog as DialogPrimitive } from 'bits-ui';
+	import { cn } from '$lib/utils';
+
+	type $$Props = DialogPrimitive.DescriptionProps;
+	let className: $$Props['class'] = undefined;
+</script>
+
+<DialogPrimitive.Description
+	class={cn('text-sm text-muted-foreground', className)}
+	{...$$restProps}
+>
+	<slot />
+</DialogPrimitive.Description>
---
+++ src/lib/components/ui/dialog/dialog-portal.svelte
@@ -0,0 +1,7 @@
+<script lang='ts'>
+    import {Dialog as DialogPrimitive} from "bits-ui"
+    // type $$
+</script>
+<DialogPrimitive.Portal {...$$restProps}>
+<slot/>
+</DialogPrimitive.Portal>
